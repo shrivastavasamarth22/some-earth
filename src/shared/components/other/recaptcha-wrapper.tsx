cc0e73e5b273daa262ca6a7ebb1b3c078e628b38
Samarth <shrivastavasamarth22@gmail.com>
2023-10-26 16:47:53 +0530

    disabled recaptcha

--- src/shared/components/other/recaptcha-wrapper.tsx
+++ src/shared/components/other/recaptcha-wrapper.tsx
@@ -1,31 +1,31 @@
-"use client";
+// "use client";
 
-import { useEffect, useState } from "react";
-import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
+// import { useEffect, useState } from "react";
+// import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 
-export default function ReCaptchaWrapper({ children }: any) {
-  const [isClient, setIsClient] = useState(false);
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
+// export default function ReCaptchaWrapper({ children }: any) {
+//   const [isClient, setIsClient] = useState(false);
+//   useEffect(() => {
+//     setIsClient(true);
+//   }, []);
 
-  if (!isClient) return null;
-  return (
-    <GoogleReCaptchaProvider
-      reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY || ""}
-      scriptProps={{
-        async: false,
-        defer: false,
-        appendTo: "head",
-        nonce: undefined,
-      }}
-      container={{
-        parameters: {
-          theme: "dark",
-        },
-      }}
-    >
-      {children}
-    </GoogleReCaptchaProvider>
-  );
-}
+//   if (!isClient) return null;
+//   return (
+//     <GoogleReCaptchaProvider
+//       reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY || ""}
+//       scriptProps={{
+//         async: false,
+//         defer: false,
+//         appendTo: "head",
+//         nonce: undefined,
+//       }}
+//       container={{
+//         parameters: {
+//           theme: "dark",
+//         },
+//       }}
+//     >
+//       {children}
+//     </GoogleReCaptchaProvider>
+//   );
+// }
