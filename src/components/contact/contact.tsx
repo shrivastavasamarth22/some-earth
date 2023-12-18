87dc7de4dc2e378cf4cd9ee640eb9d7a5399b77e
Samarth <shrivastavasamarth22@gmail.com>
2023-12-18 12:44:03 +0530

    cleaned the contacts section

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -23,103 +23,6 @@ const getTextById = (id: string) => {
 };
 
 export default function Contact() {
-    const [isSent, setIsSent] = useState(false);
-
-    const { executeRecaptcha } = useGoogleReCaptcha();
-
-    const alertDialog = useAlert({
-        Alert: AlertDialog,
-        iniAlert: { type: AlertEnum.ERROR, className: "" },
-    });
-
-    const onClick = () => {
-        try {
-            ["name", "email", "message"].forEach((id) => {
-                if (!getTextById(id)) {
-                    throw {
-                        title:
-                            id.charAt(0).toUpperCase() +
-                            id.slice(1) +
-                            " is required",
-                        description: "Please enter your " + id + " to continue",
-                        type: AlertEnum.WARNING,
-                    };
-                }
-            });
-        } catch (error) {
-            alertDialog.setAlertWithTimeout(error as any);
-        }
-    };
-
-    const loadingButton = useLoadingButton({
-        props: {
-            className:
-                "justify-center items-center rounded-full bg-dark dark:bg-white text-white dark:text-dark",
-            button: (
-                <button
-                    type="submit"
-                    disabled={isSent}
-                    onClick={onClick}
-                    className={
-                        "rounded-full flex font-normal text-sm text-center gap-3 px-6 py-3 hover:bg-neutral-500/20" +
-                        (isSent
-                            ? " bg-green-500 text-white font-medium hover:bg-green-500"
-                            : "")
-                    }
-                >
-                    {isSent ? "Sent" : "Send"}
-                    {isSent ? (
-                        <MailIcon className="w-5 h-5" strokeWidth={2} />
-                    ) : (
-                        <ArrowUpRightIcon className="w-5 h-5" />
-                    )}
-                </button>
-            ),
-        },
-    });
-
-    const handleSubmit = async (e: any) => {
-        e.preventDefault();
-
-        try {
-            loadingButton.setLoading(true);
-            if (!executeRecaptcha)
-                throw {
-                    title: "Execute recaptcha not yet available",
-                    description: "Please try again",
-                    type: AlertEnum.WARNING,
-                };
-            const name = getTextById("name");
-            const email = getTextById("email");
-            const message = getTextById("message");
-            const recaptcha_token = await executeRecaptcha("contact");
-            const res = await fetch("/api/contact", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ name, email, message, recaptcha_token }),
-            });
-            const data = await res.json();
-            if (!data.id)
-                throw {
-                    title: "Failed to send message",
-                    description: data.error ?? "Something went wrong",
-                    type: AlertEnum.ERROR,
-                };
-            setIsSent(true);
-            throw {
-                title: "Sent",
-                description: "Your message has been sent successfully",
-                type: AlertEnum.SUCCESS,
-                seconds: 5,
-            };
-        } catch (error) {
-            loadingButton.setLoading(false);
-            alertDialog.setAlertWithTimeout(error as any);
-        }
-    };
-
     function TextInput(props: any) {
         const inputProps = Object.assign({}, props);
         delete inputProps.isTextArea;
@@ -145,7 +48,7 @@ export default function Contact() {
     return (
         <>
             <div className="flex flex-col">
-                <form onSubmit={handleSubmit} className={cardStyle + " gap-7"}>
+                <form onSubmit={() => {}} className={cardStyle + " gap-7"}>
                     <CardTitle title="CONTACT" icon={<MailIcon />} />
                     <p className="text-md pb-1">
                         <a className="opacity-70">
@@ -183,10 +86,6 @@ export default function Contact() {
                         label="Message"
                         placeholder="Write your message here..."
                     />
-                    <div className="w-full flex justify-end">
-                        {loadingButton.loadingButton}
-                    </div>
-                    {alertDialog.alert}
                 </form>
                 <div className="py-3" />
                 <LinksGridCard />
