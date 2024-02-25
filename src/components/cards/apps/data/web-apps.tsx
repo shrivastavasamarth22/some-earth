09a4516406a60b807e58ac06cffa00738d65eb31
Samarth Shrivastava <shrivastavasamarth22@gmail.com>
2024-02-25 22:04:31 +0530

    Trying to resolve a ui bug with loading images

--- src/components/cards/apps/data/web-apps.tsx
+++ src/components/cards/apps/data/web-apps.tsx
@@ -5,84 +5,83 @@ import GridCard from "@/shared/components/layout/grid-card";
 import Link from "next/link";
 
 export const webApps = [
-    {
-        title: "Taskify",
-        description: "An amazing task management tool for teams of all sizes",
-        image: "/works/taskify/logo.svg",
-        link: "https://taskify-management-app.vercel.app",
-        path: "/work/taskify",
-        tags: ["Next.js", "Tailwind CSS", "React", "Node.js", "Clerk"],
-        hasBorder: true,
-    },
-    {
-        title: "Jotion",
-        description: "A Better Note Taking App.",
-        image: "/works/jotion/logo-dark.svg",
-        link: "https://github.com/shrivastavasamarth22/jotion-legacy",
-        path: "/work/jotion",
-        screenshots: [
-            "/works/jotion/screenshot-1.png",
-            // "/works/jotion/screenshot-3.png",1
-            "/works/jotion/screenshot-2.png",
-        ],
-        tags: [
-            "Next.js",
-            "Tailwind CSS",
-            "React",
-            "Node.js",
-            "ConvexDB",
-            "Clerk",
-        ],
-        hasBorder: true,
-    },
-    {
-        title: "Spotify Clone",
-        description: "A REALLY good clone of spotify.",
-        image: "/works/spotify/spotify-logo.svg",
-        link: "https://github.com/shrivastavasamarth22/spotify-clone",
-        path: "/work/spotify-clone",
-        tags: [
-            "Next.js",
-            "Tailwind CSS",
-            "Node.js",
-            "React",
-            "Clerk",
-            "Stripe",
-        ],
-        hasBorder: true,
-    },
-    {
-        title: "FreshCart",
-        description:
-            "A complete grocery app with a backend and admin dashboard.",
-        image: "/works/freshcart/freshcart-logo.png",
-        link: "https://github.com/shrivastavasamarth22/grocery-app-project",
-        path: "/work/freshcart",
-        tags: ["Bootstrap", "Python", "Flask", "SQLAlchemy", "Jinja2"],
-    },
+	{
+		title: "Taskify",
+		description: "An amazing task management tool for teams of all sizes",
+		image: "/works/taskify/logo.svg",
+		link: "https://taskify-management-app.vercel.app",
+		path: "/work/taskify",
+		tags: ["Next.js", "Tailwind CSS", "React", "Node.js", "Clerk"],
+		hasBorder: true,
+	},
+	{
+		title: "Jotion",
+		description: "A Better Note Taking App.",
+		image: "/works/jotion/logo-dark.svg",
+		link: "https://github.com/shrivastavasamarth22/jotion-legacy",
+		path: "/work/jotion",
+		screenshots: [
+			"/works/jotion/screenshot-1.png",
+			"/works/jotion/screenshot-2.png",
+		],
+		tags: [
+			"Next.js",
+			"Tailwind CSS",
+			"React",
+			"Node.js",
+			"ConvexDB",
+			"Clerk",
+		],
+		hasBorder: true,
+	},
+	{
+		title: "Spotify Clone",
+		description: "A REALLY good clone of spotify.",
+		image: "/works/spotify/spotify-logo.svg",
+		link: "https://github.com/shrivastavasamarth22/spotify-clone",
+		path: "/work/spotify-clone",
+		tags: [
+			"Next.js",
+			"Tailwind CSS",
+			"Node.js",
+			"React",
+			"Clerk",
+			"Stripe",
+		],
+		hasBorder: true,
+	},
+	{
+		title: "FreshCart",
+		description:
+			"A complete grocery app with a backend and admin dashboard.",
+		image: "/works/freshcart/freshcart-logo.png",
+		link: "https://github.com/shrivastavasamarth22/grocery-app-project",
+		path: "/work/freshcart",
+		tags: ["Bootstrap", "Python", "Flask", "SQLAlchemy", "Jinja2"],
+	},
 ];
 
 function WebAppsList() {
-    return (
-        <GridCard
-            footer={
-                <div className="w-full">
-                    <Link href="/work/#web" className="mt-1">
-                        <CardButton
-                            title="VIEW ALL"
-                            className="hover:animate-scale float-right"
-                        />
-                    </Link>
-                </div>
-            }
-            icon={<DesktopIcon />}
-            title="WEB"
-        >
-            {webApps.map((card) => (
-                <AppItem key={card.title} card={card} />
-            ))}
-        </GridCard>
-    );
+	return (
+		<GridCard
+			footer={
+				<div className="w-full">
+					<Link href="/work/#web" className="mt-1">
+						<CardButton
+							title="VIEW ALL"
+							className="hover:animate-scale float-right"
+						/>
+					</Link>
+				</div>
+			}
+			icon={<DesktopIcon />}
+			title="WEB"
+		>
+			{webApps.map((card) => (
+				<AppItem key={card.title} card={card} />
+			))}
+		</GridCard>
+	);
 }
 
 export default WebAppsList;
--- src/components/work/about-work.tsx
+++ src/components/work/about-work.tsx
@@ -9,44 +9,44 @@ import { useRouter } from "next/navigation";
 import { webApps } from "../cards/apps/data/web-apps";
 
 function AboutWork({ id }: { id: string }) {
-    const findWork = [...webApps].find((work) => work.path === `/work/${id}`);
-    const router = useRouter();
-    if (!findWork) return <NotFoundEntry />;
-    return (
-        <div className={cardStyle}>
-            <button
-                onClick={() => router.back()}
-                className="flex flex-row justify-start items-center gap-2 opacity-50 hover:opacity-100 hover:underline"
-            >
-                <div className="hover:animate-scale">
-                    <ChevronLeftIcon className="w-6 h-6" />
-                </div>
-                <p className="text-sm">BACK</p>
-            </button>
-            <div className="h-7" />
-            <AppItem card={findWork} hideLearnMore />
-            <div className="h-3" />
-            {findWork.screenshots && (
-                <div className="w-full flex flex-row justify-center gap-x-2">
-                    {findWork.screenshots.map((img, index) => (
-                        <div
-                            key={img}
-                            className="h-96 w-48 rounded-2xl bg-neutral-500/10 border border-neutral-500/10"
-                            style={{ position: "relative" }}
-                        >
-                          <Image 
-                            src={img}
-                            alt={`Screenshot ${index + 1}`}
-                            layout="fill"
-                            objectFit="cover"
-                            className="rounded-2xl"
-                          />
-                        </div>
-                    ))}
-                </div>
-            )}
-        </div>
-    );
+	const findWork = [...webApps].find((work) => work.path === `/work/${id}`);
+	const router = useRouter();
+	if (!findWork) return <NotFoundEntry />;
+	return (
+		<div className={cardStyle}>
+			<button
+				onClick={() => router.back()}
+				className="flex flex-row justify-start items-center gap-2 opacity-50 hover:opacity-100 hover:underline"
+			>
+				<div className="hover:animate-scale">
+					<ChevronLeftIcon className="w-6 h-6" />
+				</div>
+				<p className="text-sm">BACK</p>
+			</button>
+			<div className="h-7" />
+			<AppItem card={findWork} hideLearnMore />
+			<div className="h-3" />
+			{findWork.screenshots && (
+				<div className="w-full flex flex-row justify-center gap-x-2">
+					{findWork.screenshots.map((img, index) => (
+						<div
+							key={img}
+							className="h-96 w-48 rounded-2xl bg-neutral-500/10 border border-neutral-500/10"
+							style={{ position: "relative" }}
+						>
+							<Image
+								src={img}
+								alt={`Screenshot ${index + 1}`}
+								layout="fill"
+								objectFit="cover"
+								className="rounded-2xl"
+							/>
+						</div>
+					))}
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default AboutWork;
