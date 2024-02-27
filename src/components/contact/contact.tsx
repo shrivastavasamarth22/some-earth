ef661a1878405f02f60e2e1ac6192b9e7862b6e3
Samarth Shrivastava <shrivastavasamarth22@gmail.com>
2024-02-27 23:17:32 +0530

    added text-reveal-card to project

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -1,22 +1,22 @@
 "use client";
 
 import CardTitle from "@/shared/components/titles/card-title";
-import Link from "next/link";
 import LinksGridCard from "../cards/links/links-grid";
 import MailIcon from "@/assets/icons/mail";
 import cardStyle from "@/shared/styles/card";
+import { TextRevealCard } from "../ui/text-reveal-card";
 
 export default function Contact() {
     return (
         <>
             <div className="flex flex-col">
-                <div onSubmit={() => {}} className={cardStyle}>
+                <div className={cardStyle}>
                     <CardTitle title="CONTACT" icon={<MailIcon />} />
-                    <p className="text-md mb-1 mt-7">My Email:</p>
+					<p className="text-md mb-1 mt-7">My Email:</p>
                     <h2 className="text-lg md:text-2xl font-bold">
                         shrivastavasamarth22@gmail.com
                     </h2>
-                </div>
+				</div>
                 <div className="py-3" />
                 <LinksGridCard />
             </div>
